261f5405e54436dc5b6e17ce1d58dc5fbaa5055b
sabihasomapti9 <157345204+sabihasomapti9@users.noreply.github.com>
2024-03-22 15:56:49 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -44,11 +44,17 @@
 
 
 
-const imageicon = document.getElementById("image-icon");
-const optionsContainer = document.getElementById("options");
 
-imageicon.addEventListener("click", function() {
-  optionsContainer.classList.toggle("show");
-});
 
+const optionsDiv = document.getElementById("options");
+
+function showOptions() {
+  optionsDiv.classList.toggle("hidden");
+}
+
+function changeImage(imageUrl) {
+  const mainImage = document.getElementById("main-image");
+  mainImage.src = imageUrl;
+  optionsDiv.classList.add("hidden");
+}
 
