ab74fbdc33cded4812180610a66ef56f11e1dda9
sabihasomapti9 <157345204+sabihasomapti9@users.noreply.github.com>
2024-03-22 09:32:10 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
 
-function myFunction(x) {
-  x.classList.toggle("change");
-}
-const uploadImage = document.getElementById('upload-image');
-const uploadVideo = document.getElementById('upload-video');
-const uploadOptions = document.querySelector('.upload-options');
 
-uploadImage.addEventListener('click', () => {
-  uploadVideo.click();
-});
 
-uploadVideo.addEventListener('change', () => {
-  // Handle video selection (e.g., display filename)
-  const filename = uploadVideo.value.split('\\').pop(); // Adjust for different OS paths
-  console.log(`Selected video: ${filename}`);
-  
-  // Optionally show "hidden" class on upload options for visual feedback
-  uploadOptions.classList.remove('hidden');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const imageicon = document.getElementById("image-icon");
+const optionsContainer = document.getElementById("options");
+
+imageicon.addEventListener("click", function() {
+  optionsContainer.classList.toggle("show");
 });
 
 
