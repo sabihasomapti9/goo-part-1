976a957f8772bef57ba0c72a27ad0344b7727c8b
sabihasomapti9 <157345204+sabihasomapti9@users.noreply.github.com>
2024-03-23 15:52:25 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -26,17 +26,6 @@
 
 
 
-const optionsDiv = document.getElementById("options");
-
-function showOptions() {
-  optionsDiv.classList.toggle("hidden");
-}
-
-function changeImage(imageUrl) {
-  const mainImage = document.getElementById("main-image");
-  mainImage.src = imageUrl;
-  optionsDiv.classList.add("hidden");
-}
 
 
 
